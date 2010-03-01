8cd0ed90962612718d64d960724530e34cd25b84
Joe Fearnley <joe.fearnley@gmail.com>
2010-02-28 20:27:36 -0500

    still trying to figure out which api lib to use

--- twitter_py_bot.py
+++ twitter_py_bot.py
@@ -1,9 +1,13 @@
 
-import tweepy
+import twitter.api
 
-public_tweets = tweepy.api.public_timeline()
-for tweet in public_tweets:
-    print tweet.text
+twitter_search = twitter.api.Twitter(domain="search.twitter.com")
 
 
 
+s = twitter_search.search(q="hockey")
+
+for tweet in s['results']:
+    print tweet['text'] + "\n\n";
+
+#print s['results'][0]['text']
